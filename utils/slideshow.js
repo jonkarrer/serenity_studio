bd85b61672a1a1ee90ad6029a1221b5c289bafe9
jonkarrer <karrerjon@gmail.com>
2021-07-28 13:03:46 -0500

    reworking animation

--- utils/slideshow.js
+++ utils/slideshow.js
@@ -14,16 +14,10 @@ export default function slideshow() {
       slides.style.backgroundImage =
         "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url('../assets/slides/slide_two.jpg')";
       slidesTitle.innerText = ".Color.";
-      slidesTitle.style.animation = "translate_left 1s";
-      slidesScript.style.animation = "translate_down_reset 1s";
-      slidesSubtext.style.animation = "translate_up_reset 1s";
     } else {
       slides.style.backgroundImage =
         "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url('../assets/slides/slide_one.jpg')";
       slidesTitle.innerText = ".Extend.";
-      slidesTitle.style.animation = "translate_right 2s";
-      slidesScript.style.animation = "translate_down 1s";
-      slidesSubtext.style.animation = "translate_up 1s";
     }
 
     toggle = !toggle;
