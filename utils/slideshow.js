3cfed7998a029853b84f052ad33048c264a452c8
jonkarrer <karrerjon@gmail.com>
2021-07-28 13:53:01 -0500

    reworking animation

--- utils/slideshow.js
+++ utils/slideshow.js
@@ -10,7 +10,7 @@ export default function slideshow() {
     }
     if (toggle) {
       slides.style.backgroundImage =
-        "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url('../assets/slides/slide_two.jpg')";
+        "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),url('./assets/slides/slide_two.jpg')";
       slidesTitle.innerText = ".Color.";
     } else {
       slides.style.backgroundImage =
